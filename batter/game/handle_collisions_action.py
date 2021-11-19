6888fc711cbabfc20cefa9720538a41d4b2bc9b7
Alma Camarillo <allealle0303@gmail.com>
2021-11-18 17:34:06 -0700

    final version! YAY!

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -1,3 +1,4 @@
+import sys
 import random
 from game import constants
 from game.point import Point
@@ -57,18 +58,28 @@ class HandleCollisionsAction(Action):
         elif ball_position.get_y() == constants.MAX_Y -1 or ball_position.get_y() == 1:
             old_velocity = ball.get_velocity()
             new_velocity = Point((old_velocity.get_y() * -1), old_velocity.get_x())
-            ball.set_velocity(new_velocity)     
+            ball.set_velocity(new_velocity)
+            sys.exit(print("""
+
+             >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+             >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>               
+                            GAME OVER
+            <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            
+            """
+                           ))   
         #BRICKS CODE
         for brick in bricks:
             if ball.get_position().equals(brick.get_position()):
                 # remove brick and have it bounce back 
                 bricks.remove(brick)
+                
                 # have it bounce off now - use reverse method from  point
+                ball.set_velocity(ball.get_velocity().reverse())
+                # ball.set_position(ball.get_position().reverse())
                 # write code here
-                if ball_position.get_y() == constants.MAX_Y -1 or ball_position.get_y() == 1:
-                    old_velocity = ball.get_velocity()
-                    new_velocity = Point((old_velocity.get_x()), old_velocity.get_y() * -1)
-                    ball.set_velocity(new_velocity)
+                
         
         
         
